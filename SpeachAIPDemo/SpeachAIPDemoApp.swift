bd60c1d39decc9dfaeb2b7fec9ad2751025437aa
Danny Shen <shenfive@gmail.com>
2025-06-24 17:32:10 +0800

    Initial Commit

---
+++ SpeachAIPDemo/SpeachAIPDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeachAIPDemoApp.swift
+//  SpeachAIPDemo
+//
+//  Created by DannyShen on 2025/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SpeachAIPDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
